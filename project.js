26a6686e9e3e0384d00b16d0cec9ab2f5adec017
Andrew Hobden <andrew@hoverbear.org>
2013-11-26 21:50:57 -0800

    Minimum size on radius.

--- project.js
+++ project.js
@@ -6,9 +6,8 @@ document.addEventListener('DOMContentLoaded', function () {
     // This is basically fun.
     var snap = new Snap(window.innerWidth, window.innerHeight),
         bug = snap.circle(100, 100, 50);
-    
     bug.click(function () {
-        var r = Math.floor(Math.random() * (window.innerWidth / 4)),
+        var r = Math.floor(Math.random() * (window.innerWidth / 4)) + 10,
             cx = Math.floor(Math.random() * (window.innerWidth - (r * 2))) + r,
             cy = Math.floor(Math.random() * (window.innerHeight - (r * 2))) + r,
             time = Math.floor(Math.random() * 600 + 100),
